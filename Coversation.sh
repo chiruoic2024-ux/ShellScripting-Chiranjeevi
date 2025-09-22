d9651d6c21a50eb77ce8e69eeb28297ca1a3b03f
chiranjeevi <chiruoic2024@gmail.com>
2025-09-23 00:06:27 +0530

    commiting changs after creating conversation.sh

---
+++ Coversation.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+echo "Ramesh:: Hello Suresh how are you"
+echo "Suresh:: Hi Ramesh I am good, How are you"
+echo "Ramesh:: what are you learning recently"
+echo "Suresh:: I am learning shell scripting"
