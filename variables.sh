21850f08e20c82efcdbf55699fd3edc51d123f68
chiranjeevi <chiruoic2024@gmail.com>
2025-09-23 00:22:25 +0530

    commiting changs in variables.sh

---
+++ variables.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+PERSON1=CHIRANJEEVI
+PERSON2=RAVI
+echo "$PERSON1:: Hello $PERSON2 how are you"
+echo "$PERSON2:: Hi $PERSON1 I am good, How are you"
+echo "$PERSON1:: what are you learning recently"
+echo "$PERSON2:: I am learning shell scripting"
+
+
+
