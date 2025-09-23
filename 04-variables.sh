35a5a1d2c1411f21cc5786b2e23681610769f6c3
chiranjeevi <chiruoic2024@gmail.com>
2025-09-23 11:13:27 +0530

    Commiting changes for 04-variables.sh

---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1:: Hello $PERSON2 how are you"
+echo "$PERSON2:: Hi $PERSON1 I am good, How are you"
+echo "$PERSON1:: what are you learning recently"
+echo "$PERSON2:: I am learning shell scripting"
