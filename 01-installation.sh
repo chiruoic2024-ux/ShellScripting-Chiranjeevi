e7ca1421da8a3038d5a74b3aca3609c0f75c2bc3
chiranjeevi <chiruoic2024@gmail.com>
2025-09-23 21:37:45 +0530

    installation1 committed

--- 01-installation.sh
+++ 01-installation.sh
@@ -4,12 +4,14 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0]; then
     echo "ERROR::Install MySql software with root user privileges"
+    exit 1 # failure is other than 0
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "ERROR:: MySql installation is failed"
+    exit 1 # failure is other than 0
 else
     echo "Mysql installation is successful"
 fi
