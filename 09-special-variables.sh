94a8ba0e369ac3ed5717d21ea9cd8353cf6d20b7
chiranjeevi <chiruoic2024@gmail.com>
2025-09-23 16:51:41 +0530

    special variables commited

--- 09-special-variables.sh
+++ 09-special-variables.sh
@@ -3,3 +3,6 @@
 echo "All variables are passed to the sript: $@"
 echo "All variables are passed to the sript: $*"
 echo "Script name is: $0"
+echo "Current directory is: $PWD"
+echo "Who is running this: $USER"
+echo "home directory of the user: $HOME"
