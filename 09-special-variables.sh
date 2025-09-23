6496900e83ee94062d0bd0c32b997c28b380d584
chiranjeevi <chiruoic2024@gmail.com>
2025-09-23 17:02:13 +0530

    special variables commited

--- 09-special-variables.sh
+++ 09-special-variables.sh
@@ -7,3 +7,5 @@ echo "Current directory is: $PWD"
 echo "Who is running this: $USER"
 echo "home directory of the user: $HOME"
 echo "PID of this script: $$"
+sleep 50 &
+echo "PID of the last command in background is: $!"
