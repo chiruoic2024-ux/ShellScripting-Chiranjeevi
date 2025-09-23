8cf03585f36b7c19f193cda61c4aa7aab85bf3aa
chiranjeevi <chiruoic2024@gmail.com>
2025-09-23 20:13:12 +0530

    conditions1 committed

--- 01-conditions.sh
+++ 01-conditions.sh
@@ -4,7 +4,10 @@ NUMBER1=$1
 
 if [ $NUMBER1 -lt 10 ]; then
     echo "Given number $NUMBER1 is less than 10"
-    else
-        echo "Given number $NUMBER1 is greater than or equal to 10"
-        fi
+elif [ $NUMBER1 -eq 10]; then
+    echo "Given number $NUMBER1 is equal to 10"
+
+else
+    echo "Given number $NUMBER1 is greater than 10"
+fi
  
