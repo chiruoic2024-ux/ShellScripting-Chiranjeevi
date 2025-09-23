ad565ba4cd331574ef4a79475157e466f3be090a
chiranjeevi <chiruoic2024@gmail.com>
2025-09-23 19:59:37 +0530

    conditions1 committed

---
+++ 01-conditions.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+NUMBER1=$1
+
+if [ $NUMBER1 -lt 10 ]; then
+    echo "Given number $NUMBER1 is less than 10"
+    else
+        echo "Given number $NUMBER1 is greater than or equal to 10"
+        fi
+ 
