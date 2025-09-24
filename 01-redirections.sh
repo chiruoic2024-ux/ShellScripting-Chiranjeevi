1eb2a990b19763d137d5672f74c9513a47429bc4
chiranjeevi <chiruoic2024@gmail.com>
2025-09-24 16:14:59 +0530

    redirections

---
+++ 01-redirections.sh
@@ -0,0 +1,50 @@
+#! /bin/bash
+USER_ID=$(id -u)
+R="\e[31m "
+G="\e[32m "
+Y="\e[33m "
+N="\e[0m  "
+
+LOGS_DIR="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_DIR/$SCRIPT_NAME/.log"
+mkdir -p $LOGS_DIR
+echo "Script execution started at: $(date)"
+if [ $USER_ID -ne 0]; then
+    echo "ERROR::Install the softwares using root user priveleges!"
+    exit 1 # failure is other than 0
+fi
+
+VALIDATE(){ # Functions receives inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+    echo -e "Installing $2 .....$R Failure $N"
+    exit 1 # failure is other than 0
+    else
+    echo "Installing $2 .....$R Success $N"
+    fi
+
+}
+dnf list installed mysql &>>$LOG_FILE
+#Install if it is not found
+if [ $? -ne 0 ]; then
+dnf install mysql -y &>>$LOG_FILE
+VALIDATE $? "MySQL"
+else    
+    echo -e "MySQL already exist....$G SKIPPING $N"
+fi
+
+dnf list installed nginx &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+dnf install nginx -y &>>$LOG_FILE
+VALIDATE $? "Nginx"
+else    
+    echo -e "Nginx already exist....$G SKIPPING $N"
+fi
+
+dnf list installed python3 &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+dnf install python3 -y &>>$LOG_FILE
+VALIDATE $? "python3"
+else    
+    echo -e "Python3 already exist....$G SKIPPING $N"
+fi
