2a54ba1fd6edf83b3bb7cc6f1d27f5886d8063e4
chiranjeevi <chiruoic2024@gmail.com>
2025-09-24 20:55:33 +0530

    loops2

--- 02-loops.sh
+++ 02-loops.sh
@@ -1,27 +1,36 @@
 #! /bin/bash
-# I have a requirement to install mysql, nginx and mongodb-mongosh softwares in linux servers
-USERID=$(id -u)
+USER_ID=$(id -u)
 R="\e[31m "
 G="\e[32m "
 Y="\e[33m "
 N="\e[0m  "
 
-if [ $USERID -ne 0 ]; then
-    echo "ERROR::Install MySql software with root user privileges"
+LOGS_DIR="/var/log/shell-script"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_DIR/$SCRIPT_NAME.log"
+mkdir -p $LOGS_DIR
+echo "Script execution started at: $(date)" | tee -a $LOG_FILE
+if [ $USER_ID -ne 0 ]; then
+    echo "ERROR::Install the softwares using root user priveleges!"
     exit 1 # failure is other than 0
 fi
 
 VALIDATE(){ # Functions receives inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
-    echo -e "Installing $2 .....$R Failure $N"
+    echo -e "Installing $2 .....$R Failure $N" | tee -a $LOG_FILE
     exit 1 # failure is other than 0
     else
-    echo "Installing $2 .....$R Success $N"
+    echo -e "Installing $2 .....$G Success $N" | tee -a $LOG_FILE
     fi
 
 }
-
+# $@ is for all special variables (it returns all special variables--what ever we send args it returns)
 for package in $@
 do
-    echo "Package is: $package"
+    if [ $? -ne 0 ];then #if exist status is not equal to 0 install software
+    dnf install $package -y &>>LOG_FILE
+    VALIDATE $? "$package"
+    else
+        echo "$package alerady exists ......$Y SKIPPING $N"
+    fi
 done
