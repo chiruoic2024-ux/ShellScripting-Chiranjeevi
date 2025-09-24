2710c6a4aa0e69d21167c6423646f72760af2061
chiranjeevi <chiruoic2024@gmail.com>
2025-09-24 14:37:21 +0530

    Colors testing

---
+++ 01-colors.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# set colors for scripts
+
+R="\e[31m "
+G="\e[32m "
+Y="\e[33m "
+
+echo -e " $R Hello world!"
+echo -e " $G Hello world!"
+echo -e " $Y Hello world!"
+echo "Testing"
