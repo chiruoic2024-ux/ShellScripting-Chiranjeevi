92e9622381e5f9ec08469a8bef945576bc039537
chiranjeevi <chiruoic2024@gmail.com>
2025-10-02 13:13:37 +0530

    commit set.sh

--- set.sh
+++ set.sh
@@ -6,7 +6,7 @@ set -e
 # {
 #     echo "There is an error"
 # }
-trap 'echo "Thre is a error in $LINENO and command is $BASH_COMMAND"' ERR
+trap echo "Thre is a error in $LINENO and command is $BASH_COMMAND" ERR
 echo "Hello"
 echo "Before error"
 jkajdskjklsjdklj  # Here Shell understands that there is an error and the signal is ERR
